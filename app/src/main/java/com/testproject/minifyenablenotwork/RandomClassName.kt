5ca1a2ae9e50e5a28183026fba54dd80c1b1137e
d.verentsov <decsent@yandex.ru>
2022-08-17 15:40:44 +0300

    Init commit.

---
+++ app/src/main/java/com/testproject/minifyenablenotwork/RandomClassName.kt
@@ -0,0 +1,16 @@
+package com.testproject.minifyenablenotwork
+
+import kotlin.random.Random
+
+class RandomClassName {
+
+    fun simpleMethod() {
+        "print line".forEach {
+            print(it)
+        }
+    }
+
+    fun anotherMethod(): Int {
+        return Random.nextInt()
+    }
+}
---
+++ app/src/main/java/com/testproject/minifyenablenotwork/RandomObject.kt
@@ -0,0 +1,10 @@
+package com.testproject.minifyenablenotwork
+
+object RandomObject {
+
+    val randomClass = RandomClassName()
+
+    fun proxyMethod() {
+        randomClass.anotherMethod()
+    }
+}
